547e370cd381d66b36caedb20ecd5c0259ad26da
Vũ Vương Quốc Anh <quanhuk0903@gmail.com>
2023-02-07 20:04:27 +0700

    Add target framerate

---
+++ Assets/Assets/Scripts/System/Setting.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setting : MonoBehaviour
+{
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
